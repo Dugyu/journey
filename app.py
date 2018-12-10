ff52f7c0e8b24e2116f692d30c304253270d2373
Dugyu <yxylyzy@126.com>
2018-12-09 19:31:34 -0500

    remove manager

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
 from journeyapp import app
-from flask_script import Manager
-from flask_migrate import MigrateCommand
 
-manager = Manager(app)
-manager.add_command('db', MigrateCommand)
 
 
 if __name__ == "__main__":
-    manager.run()
     app.run(debug=False)
