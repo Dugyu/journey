24cb6a3c0fe46f3e055fcb7ac856ad0639b7a688
Dugyu <yxylyzy@126.com>
2018-12-12 13:40:36 -0500

    update gitignore

--- journeyapp/__init__.py
+++ journeyapp/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_session import Session
-
+from flask_s3 import FlaskS3
 from tempfile import mkdtemp
 
 
@@ -21,13 +21,17 @@ Session(app)
 
 
 # Configure database
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://omfgiyhxovsdia:85689f7ebbec3b5a3b901287465ae865c2aff766454a9ca9027e55323992d757@ec2-54-243-150-10.compute-1.amazonaws.com:5432/d91d5sfeo9tll2'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://omfgiyhxovsdia:85689f7ebbec3b5a3b901287465ae865c2aff766454a9ca9027e55323992d757@ec2-54-243-150-10.compute-1.amazonaws.com:5432/d91d5sfeo9tll2'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
 # Database
 db = SQLAlchemy(app)
+
+# S3 bucket
+app.config['FLASKS3_BUCKET_NAME'] = 'solidarityjourney'
+s3 = FlaskS3(app)
 
 def strlen(s):
     return len(s)
